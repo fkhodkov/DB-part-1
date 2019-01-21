fa8d260f5317956a82f3d2ded7b37afbafec8479
Fedor Khodkov <fkhodkov@fkhodkov.info>
2019-01-21 14:55:04 +0300

    Renaming

--- sql/2-city_translation.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS city_translation;
-
-SELECT city.city_id, external_city.city_id AS ext INTO city_translation
-  FROM city RIGHT JOIN external_city USING (name);
-
-ALTER table city_translation ADD PRIMARY KEY (ext);
-
-UPDATE city_translation
-   SET city_id = nextval('external_city_seq') WHERE city_id is NULL;
--- sql/2.4-copy_functions.sql
+++ sql/2.4-copy_functions.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS city_translation;
+
+SELECT city.city_id, external_city.city_id AS ext INTO city_translation
+  FROM city RIGHT JOIN external_city USING (name);
+
+ALTER table city_translation ADD PRIMARY KEY (ext);
+
+UPDATE city_translation
+   SET city_id = nextval('external_city_seq') WHERE city_id is NULL;
+
 DROP FUNCTION IF EXISTS copy_city;
 CREATE FUNCTION copy_city(starting INTEGER) RETURNS VOID AS $$
   DECLARE
